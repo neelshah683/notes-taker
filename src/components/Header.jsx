0ccf68e7173c89c427e883c34ee44334b66525ac
NEEL <neelshah682003@gmail.com>
2024-02-02 22:20:20 +0530

    deploy to github

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HighlightIcon from '@mui/icons-material/Highlight';
+
+function Header() {
+  return (
+    <header>
+        <h2>
+          <HighlightIcon />
+          Notes Taker
+        </h2>
+    </header>
+  );
+}
+export default Header;
